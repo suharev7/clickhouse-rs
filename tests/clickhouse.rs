ece4432ae8c54978348d964d8d21f61e47085622
deniallugo <deniallugo@gmail.com>
2019-11-20 15:57:05 +0300

    Separate tests

--- tests/clickhouse.rs
+++ tests/clickhouse.rs
@@ -753,9 +753,9 @@ async fn test_binary_string() -> Result<(), Error> {
 
 #[cfg(feature = "tokio_io")]
 #[tokio::test]
-async fn test_enum() -> Result<(), Error> {
+async fn test_enum_16() -> Result<(), Error> {
 	let ddl = "
-        CREATE TABLE IF NOT EXISTS clickhouse_Enum (
+        CREATE TABLE IF NOT EXISTS clickhouse_enum (
             enum_16_row        Enum16(
                                 'zero' = 5,
                                 'first' = 6
@@ -765,10 +765,45 @@ async fn test_enum() -> Result<(), Error> {
 	let query = "
         SELECT
             enum_16_row
-        FROM clickhouse_Enum";
+        FROM clickhouse_enum";
 
 	let block = Block::new()
 		.column("enum_16_row", vec![Enum::of(5), Enum::of(6)]);
+
+	let pool = Pool::new(database_url());
+	let mut c = pool.get_handle().await?;
+	c.execute("DROP TABLE IF EXISTS clickhouse_enum")
+		.await?;
+	c.execute(ddl).await?;
+	c.insert("clickhouse_enum", block).await?;
+	let block = c.query(query).fetch_all().await?;
+
+	let enum_8_a: Enum = block.get(0, "enum_16_row")?;
+	let enum_8_b: Enum = block.get(1, "enum_16_row")?;
+
+	assert_eq!(2, block.row_count());
+	assert_eq!(vec!([Enum::of(5), Enum::of(6)]), vec!([enum_8_a, enum_8_b]));
+
+	Ok(())
+}
+
+#[tokio::test]
+async fn test_enum_8() -> Result<(), Error> {
+	let ddl = "
+        CREATE TABLE IF NOT EXISTS clickhouse_Enum (
+            enum_8_row        Enum8(
+                                'zero' = 1,
+                                'first' = 2
+                          )
+        ) Engine=Memory";
+
+	let query = "
+        SELECT
+            enum_8_row
+        FROM clickhouse_Enum";
+
+	let block = Block::new()
+		.column("enum_8_row", vec![Enum::of(1), Enum::of(2)]);
 
 	let pool = Pool::new(database_url());
 	let mut c = pool.get_handle().await?;
@@ -778,8 +813,8 @@ async fn test_enum() -> Result<(), Error> {
 	c.insert("clickhouse_Enum", block).await?;
 	let block = c.query(query).fetch_all().await?;
 
-	let enum_8_a: Enum = block.get(0, "enum_16_row")?;
-	let enum_8_b: Enum = block.get(1, "enum_16_row")?;
+	let enum_8_a: Enum = block.get(0, "enum_8_row")?;
+	let enum_8_b: Enum = block.get(1, "enum_8_row")?;
 
 	assert_eq!(2, block.row_count());
 	assert_eq!(vec!([Enum::of(5), Enum::of(6)]), vec!([enum_8_a, enum_8_b]));

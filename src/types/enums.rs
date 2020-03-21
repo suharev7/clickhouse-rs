293d28a73fe2eee56ed215128d9402f505e6a3cc
deniallugo <deniallugo@gmail.com>
2020-03-21 12:12:33 +0300

    Insert with hardcoded enum

--- src/types/enums.rs
+++ src/types/enums.rs
@@ -4,32 +4,36 @@ use std::fmt;
 pub struct Enum8(pub(crate) i8);
 
 impl Default for Enum8 {
-	fn default() -> Self {
-		Self(0)
-	}
+    fn default() -> Self {
+        Self(0)
+    }
 }
 
 
 impl PartialEq for Enum8 {
-	fn eq(&self, other: &Self) -> bool {
-		self.0 == other.0
-	}
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
 }
 
 impl fmt::Display for Enum8 {
-	fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-		write!(f, "Enum8({})", self.0)
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(f, "Enum8({})", self.0)
+    }
 }
 
 impl fmt::Debug for Enum8 {
-	fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-		write!(f, "Enum8({})", self.0)
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(f, "Enum8({})", self.0)
+    }
 }
 
 impl Enum8 {
-	pub fn of(source: i8) -> Self {
-		Self(source)
-	}
+    pub fn of(source: i8) -> Self {
+        Self(source)
+    }
+    #[inline(always)]
+    pub fn internal(&self) -> i8 {
+        self.0
+    }
 }
